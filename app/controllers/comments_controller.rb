bb8ed54496d9d4bb974e6c3c1af0343c76410491
Maximiliano Bernard <bmaximiliano0210@gmail.com>
2024-05-05 00:27:12 -0300

    Fix comment on posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,16 +3,28 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     @comment.user = current_user
-    if @comment.save
-      redirect_to post_path(@post)
-    else
-      render 'posts/show'
+    respond_to do |format|
+      if @comment.save
+        format.turbo_stream
+        format.html { redirect_to post_path(@post) }
+      else
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(@comment, partial: 'comments/form', locals: { comment: @comment })
+        end
+        format.html { render 'posts/show' }
+      end
     end
   end
 
   def new
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@comment.post)
   end
 
   private
