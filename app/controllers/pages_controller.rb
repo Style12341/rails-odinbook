65834468d1664d1c6677f30fb776e26d89936c71
Maximiliano Bernard <bmaximiliano0210@gmail.com>
2024-05-05 15:09:48 -0300

    Add likes counter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   include Pagy::Backend
   def home
-    @pagy, @posts = pagy(current_user.feed.includes(:user).includes(:likes).includes(:comments), items: 5)
+    @pagy, @posts = pagy(current_user.feed.includes(:user).includes(:comments), items: 5)
     render 'scrollable_content' if params[:page]
   end
 end
