1521eda36ed3e00ebd6fdf160165b57e7c0ea47a
Maximiliano Bernard <bmaximiliano0210@gmail.com>
2024-05-07 19:08:38 -0300

    Add Follow list on user page Refactor this for more efficient db use

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -3,4 +3,10 @@ class Follow < ApplicationRecord
   # belongs_to :followed, class_name: 'User', foreign_key: :followed_id
   belongs_to :follower, class_name: 'User', foreign_key: :follower_id
   belongs_to :followee, class_name: 'User', foreign_key: :followee_id
+  validates_uniqueness_of :follower_id, scope: :followee_id
+
+  def self.follow(follower, followee)
+    Follow.find_by(follower:, followee:)
+  end
+  
 end
