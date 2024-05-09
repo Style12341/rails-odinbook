d6d2040e08e5723ddbe736df0af26ea2776f4795
Maximiliano Bernard <bmaximiliano0210@gmail.com>
2024-05-09 02:22:57 -0300

    Add db migration in render build

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "odinbook_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'dr-indo.com',
+    port: 465,
+    domain: 'https://mysite-pm0k.onrender.com',
+    user_name: 'no-reply@dr-indo.com',
+    password: Rails.application.credentials.dig(:smtp_password),
+    authentication: 'plain',
+    enable_starttls: true,
+    open_timeout: 5,
+    read_timeout: 5
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
